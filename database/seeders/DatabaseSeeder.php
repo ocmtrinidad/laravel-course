1a7deffdfb9431016f1712eb2141ed22ef16801d
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-12 11:16:09 +0800

    Setup

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\PostSeeder;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('testpassword'),
+        ]);
+
+        $this->call([
+            CategorySeeder::class,
+            PostSeeder::class,
+        ]);
+    }
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+
+                    <ul
+                        class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 justify-center">
+                        <li class="me-2">
+                            <a href="#" class="inline-block px-4 py-3 text-white bg-blue-600 rounded-lg active"
+                                aria-current="page">Tab 1</a>
+                        </li>
+                        <li class="me-2">
+                            <a href="#"
+                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab
+                                2</a>
+                        </li>
+                        <li class="me-2">
+                            <a href="#"
+                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab
+                                3</a>
+                        </li>
+                        <li class="me-2">
+                            <a href="#"
+                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab
+                                4</a>
+                        </li>
+                        <li>
+                            <a class="inline-block px-4 py-3 text-gray-400 cursor-not-allowed dark:text-gray-500">Tab
+                                5</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
