9c6e22500398fc1c117da32cf2652baff83a4b89
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-12 13:00:43 +0800

    Updated index(). Returns categories and posts.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::get();
+        $posts = Post::orderBy("created_at", "desc")->paginate(5);
+        return view("dashboard", ["categories" => $categories, "posts" => $posts]);
     }
 
     /**
