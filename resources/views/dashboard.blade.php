3f5d5185183019649991de59bc940b56c2aa5ca1
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-14 12:06:10 +0800

    Called components.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,26 +3,18 @@
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-4 text-gray-900">
-                    <ul
-                        class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 justify-center">
-                        <li class="me-2">
-                            <a href="#"
-                                class="inline-block px-4 py-2 text-white bg-blue-600 rounded-lg active">All</a>
-                        </li>
-                        @foreach ($categories as $category)
-                            <li class="me-2">
-                                <a href="#"
-                                    class="inline-block px-4 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">{{ $category->name }}</a>
-                            </li>
-                        @endforeach
-                    </ul>
+                    {{-- Passing value to component key=value. If passing a variable, prefix with : --}}
+                    <x-category-tabs>
+                        {{-- Passing values inside tags will be output as {{slot}} --}}
+                        No Categories
+                    </x-category-tabs>
                 </div>
             </div>
 
             <div class="mt-8">
                 <div>
                     @forelse ($posts as $post)
-                        {{-- Passing $post to component --}}
+                        {{-- Passing a variable to a component --}}
                         <x-post-item :post="$post" />
                     @empty
                         <div class="text-center">
