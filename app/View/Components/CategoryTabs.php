61eebce4cf186bf35219984ceede5351c0415ab2
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-14 12:05:23 +0800

    Created component for displaying categories

---
+++ app/View/Components/CategoryTabs.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Models\Category;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class CategoryTabs extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    // public string $variable makes value accessible in component.
+    public function __construct() {}
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        $categories = Category::get();
+        return view('components.category-tabs', ["categories" => $categories]);
+    }
+}
