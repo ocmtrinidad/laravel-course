6ce755f4023b5887d80ad9a14abdf97ec5cb3bd5
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-15 13:34:04 +0800

    Added {{slot}}

--- resources/views/components/input-textarea.blade.php
+++ resources/views/components/input-textarea.blade.php
@@ -4,4 +4,5 @@
 {{-- ->merge then combines all the attributes into a class. For example, this component was called in ../post/create.blade.php with a class="block mt-1 w-full". This class will be combined with the class included in the merge. --}}
 <textarea @disabled($disabled)
     {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+    {{ $slot }}
 </textarea>
