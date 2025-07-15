96d330f29e3dd66b255aa96cc1f668b5813412b8
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-15 10:58:01 +0800

    PostController methods

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -24,7 +24,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view("post.create");
     }
 
     /**
@@ -32,7 +32,7 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        dd($request->all());
     }
 
     /**
