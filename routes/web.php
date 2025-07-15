74ded650f66f65c1a3dc311d59f616c23a8e5219
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-15 10:57:06 +0800

    Created routes for PostControllers.

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get(
-    '/',
-    [PostController::class, "index"]
-)->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(["auth", "verified"])->group(function () {
+    Route::get('/', [PostController::class, "index"])
+        ->name('dashboard');
+    Route::get("/post/create", [PostController::class, "create"])
+        ->name("post.create");
+    Route::post("/post/create", [PostController::class, "store"])
+        ->name("post.store");
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__ . '/auth.php';
