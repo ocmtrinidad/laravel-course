a0bb29ad3777966e2037767f7ff1aacfc8ce0803
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-16 10:15:16 +0800

    Added username, image, and bio to $fillable.

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     protected $fillable = [
         'name',
+        'username',
+        'image',
+        'bio',
         'email',
         'password',
     ];
