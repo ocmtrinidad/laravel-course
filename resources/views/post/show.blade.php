b01e7e3bd27ce8e541f95beb23889ef72d46ad5b
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-17 10:01:17 +0800

    Imported <x-avatar/>

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -4,14 +4,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-8">
                 <h1 class="text-2xl mb-4">{{ $post->title }}</h1>
                 <div class="flex gap-4">
-                    @if ($post->user->image)
-                        <img class="w-12 h-12 rounded-full" src="{{ $post->user->imageUrl() }}"
-                            alt="{{ $post->user->username }}">
-                    @else
-                        <img class="w-12 h-12 rounded-full"
-                            src="https://w7.pngwing.com/pngs/510/812/png-transparent-organization-computer-icons-avatar-company-information-profile-miscellaneous-angle-company.png"
-                            alt="Default Avatar">
-                    @endif
+                    <x-user-avatar :user="$post->user" />
                     <div>
                         <div class="flex gap-2">
                             <a href="{{ route('profile.show', ['user' => $post->user]) }}"
