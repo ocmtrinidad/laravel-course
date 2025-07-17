e5f4ed626b195621dd07e8e4ffd31aa3ca07b588
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-17 13:31:52 +0800

    Created Follower model

---
+++ app/Models/Follower.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Follower extends Model
+{
+    /** @use HasFactory<\Database\Factories\FollowerFactory> */
+    use HasFactory;
+
+    protected const UPDATED_AT = null;
+
+    protected $fillable = [
+        "user_id",
+        "follower_id"
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function follower()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
