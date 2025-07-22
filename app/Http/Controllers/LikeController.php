0d838c71bac0df7607dfaeb47eb6038101621562
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-22 10:43:29 +0800

    Created LikeController. Has like() that creates or deletes a Like based on post and user.

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LikeController extends Controller
+{
+    public function like(Post $post)
+    {
+        $liked = false;
+        $hasLiked = $post->likes()->where("user_id", Auth::user()->id)->exists();
+        if (!$hasLiked) {
+            $post->likes()->create([
+                "user_id" => Auth::user()->id
+            ]);
+            $liked = true;
+        } else {
+            $post->likes()->where("user_id", Auth::user()->id)->delete();
+        }
+
+        return response()->json([
+            "likesCount" => $post->likes()->count(),
+            "liked" => $liked
+        ]);
+    }
+}
