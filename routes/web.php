28272b6284742b7c4954eda8244d3a7b24320492
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-22 09:40:05 +0800

    Created new route for liking posts

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FollowerController;
+use App\Http\Controllers\LikeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PublicProfileController;
@@ -22,9 +23,12 @@ Route::middleware(["auth", "verified"])->group(function () {
     Route::get("/@{username}/{post:slug}", [PostController::class, "show"])->name("post.show");
 
     Route::post("/follow/{user}", [FollowerController::class, "toggleFollow"])->name("follow");
+
+    Route::post("/like/{post}", [LikeController::class, "like"])->name("like");
 });
 
 Route::get("/@{user:username}", [PublicProfileController::class, "show"])->name("profile.show");
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
