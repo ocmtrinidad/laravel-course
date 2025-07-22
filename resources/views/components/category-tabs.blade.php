08b156788264726445f7a6987636e2da04901cf3
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-22 15:03:51 +0800

    Added functionality for styling selected tab

--- resources/views/components/category-tabs.blade.php
+++ resources/views/components/category-tabs.blade.php
@@ -1,12 +1,21 @@
 <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 justify-center">
     <li class="me-2">
-        <a href="#" class="inline-block px-4 py-2 text-white bg-blue-600 rounded-lg active">All</a>
+        {{-- request('model') gets the binded route model in web.php. --}}
+        <a href="{{ route('dashboard') }}"
+            class="{{ request('category')
+                ? ' inline-block px-4 py-2'
+                : ' inline-block px-4 py-2 text-white bg-blue-600 rounded-lg active' }}">
+            All
+        </a>
     </li>
     @forelse ($categories as $category)
         <div>
             <li class="me-2">
-                <a href="#"
-                    class="inline-block px-4 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">
+                {{-- Route::currentRouteNamed('route_name') checks if url is the same as route_name --}}
+                <a href="{{ route('post.byCategory', $category) }}"
+                    class="{{ Route::currentRouteNamed('post.byCategory') && request('category')->id == $category->id
+                        ? 'inline-block px-4 py-2 text-white bg-blue-600 rounded-lg active'
+                        : 'inline-block px-4 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white' }}">
                     {{ $category->name }}
                 </a>
             </li>
