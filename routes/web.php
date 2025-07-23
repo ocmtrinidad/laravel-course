5b0dc5d444167fc081b53fc0584560d11cadcb2d
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-23 08:47:00 +0800

    Moved post.byCategory route out of auth, verified middleware

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,13 @@ Route::get('/', [PostController::class, "index"])
 // Calls show(string username, Post post)
 // post:slug means url will display slug
 Route::get("/@{username}/{post:slug}", [PostController::class, "show"])->name("post.show");
+Route::get("/category/{category:name}", [PostController::class, "category"])->name("post.byCategory");
 
 Route::middleware(["auth", "verified"])->group(function () {
     Route::get("/post/create", [PostController::class, "create"])
         ->name("post.create");
     Route::post("/post/create", [PostController::class, "store"])
         ->name("post.store");
-
-    Route::get("/category/{category:name}", [PostController::class, "category"])->name("post.byCategory");
 
     Route::post("/follow/{user}", [FollowerController::class, "toggleFollow"])->name("follow");
 
