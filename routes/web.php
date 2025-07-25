5bf1d3137195dfe14e640f98b5f79e7648d66d27
Oliver Trinidad <ocmtrinidad@gmail.com>
2025-07-25 10:54:34 +0800

    Update

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,6 @@ Route::middleware(["auth", "verified"])->group(function () {
 
 Route::get("/@{user:username}", [PublicProfileController::class, "show"])->name("profile.show");
 
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
